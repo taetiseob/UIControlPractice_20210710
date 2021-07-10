6f979de5644f6483117c699ebe84344179ee9fe6
김태섭 <kyoer234@gmail.com>
2021-07-10 11:51:02 +0900

    editText 값을 토스트로 띄움

--- app/src/main/java/com/nepplus/uicontrolpractice_20210710/MainActivity.kt
+++ app/src/main/java/com/nepplus/uicontrolpractice_20210710/MainActivity.kt
@@ -2,10 +2,20 @@ package com.nepplus.uicontrolpractice_20210710
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //토스트 버튼이 눌리면 ( 이벤트 처리)
+        toastBtn.setOnClickListener {
+            //contentEdt에 적혀있는 내용을 가져와
+            val inputContent = contentEdt.text.toString()
+            //토스트 문구로 사용.
+            Toast.makeText(this, inputContent, Toast.LENGTH_SHORT).show()
+        }
     }
 }
